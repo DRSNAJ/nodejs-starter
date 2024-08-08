c6c4a3d0605a92375f5974c033d2aec6787ffc12
Randike Jayathilake <randike@corzent.com>
2024-08-08 15:01:04 +0530

    winston logger configured with more logs

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -6,6 +6,7 @@ import { Strategy as JwtStrategy } from 'passport-jwt';
 import { ExtractJwt as ExtractJwt } from 'passport-jwt';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
+import {reqLogger} from '../utils/logger';
 
 const router = express.Router();
 
@@ -22,16 +23,17 @@ async function createJwt(userId: String) {
 router.get("/", (req, res) => res.send("Hello World!"));
 
 router.post("/signup", async (req, res) => {
+    const reqId = req.log?.id || '';
     try {
         const newUser: userType = req.body;
         const addUser = new UserModel(newUser);
         await addUser.hashPassword();
         await addUser.save();
-        console.log(`New user: ${newUser.username} successfully added.`);
+        reqLogger.info(reqId,`New user: ${newUser.username} successfully added.`);
         const token = await createJwt(addUser.user_id);
         res.json({ token });
     } catch (error) {
-        console.error(error);
+        reqLogger.error(reqId,`Error adding user: ${error}`);
         res.status(422);
         res.send("Error adding user");
     }
@@ -40,35 +42,36 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
     const reqUser: userType = req.body;
 
+    const reqId = req.log?.id || '';
+
     try {
         let selectedUser = await UserModel.findOne({ "username": reqUser.username });
 
         if (!!selectedUser) {
             selectedUser = new UserModel(selectedUser);
-            console.log(`User found: ${selectedUser.username}`);
+            reqLogger.info(reqId, `User found: ${selectedUser.username}`);
 
             if (await selectedUser.verifyPassword(reqUser.password)) {
                 const token = await createJwt(selectedUser.user_id);
                 res.statusMessage = "Authorized";
                 res.json({ token });
-                console.log(`User ${selectedUser.username} authorized successfully.`);
+                reqLogger.info(reqId, `User ${selectedUser.username} authorized successfully.`);
             } else {
                 res.statusCode = 401;
                 res.statusMessage = "Unauthorized";
                 res.send("Invalid Password");
-                console.log(`Failed login attempt for user ${selectedUser.username}: Invalid Password.`);
+                reqLogger.warn(reqId, `Failed login attempt for user ${selectedUser.username}: Invalid Password.`);
             }
         } else {
             res.statusCode = 404;
             res.send("User Not Found");
-            console.log(`Failed login attempt: User ${reqUser.username} not found.`);
+            reqLogger.error(reqId, `Failed login attempt: User ${reqUser.username} not found.`);
         }
 
     } catch (error) {
-        console.error(error);
         res.statusCode = 500;
         res.send("Internal Server Error");
-        console.log(`Error during login process: ${error}`);
+        reqLogger.error(reqId, `Error during login process: ${error}`);
     }
 });
 
